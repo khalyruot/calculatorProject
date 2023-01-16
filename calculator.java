92da6a14acda72fefa71231cd3e6cbfe7251d117
Khaly_Ruot <63812639+khalyruot@users.noreply.github.com>
2023-01-16 10:43:03 -0800

    add textfield

--- calculator.java
+++ calculator.java
@@ -4,7 +4,9 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import java.util.ArrayList; // import the ArrayList class
+import javax.swing.JTextField;
+
+//import java.util.ArrayList; // import the ArrayList class
 
 public class calculator {
     public static void main(String[] arg) {
@@ -13,34 +15,44 @@ public class calculator {
         JPanel panel = new JPanel();
         panel.setLayout(new FlowLayout());
         JLabel label = new JLabel("Calculator");
+        JTextField textField = new JTextField(10);
+
         JButton button1 = new JButton();
         JButton button2 = new JButton();
         JButton button3 = new JButton();
+        JButton button4 = new JButton();
+        JButton button5 = new JButton();
+        JButton button6 = new JButton();
+        JButton button7 = new JButton();
+        JButton button8 = new JButton();
+        JButton button9 = new JButton();
         button1.setText("1");
         button2.setText("2");
         button3.setText("3");
+        button4.setText("4");
+        button5.setText("5");
+        button6.setText("6");
+        button7.setText("7");
+        button8.setText("8");
+        button9.setText("9");
         panel.add(label);
+        panel.add(textField);
         panel.add(button1);
         panel.add(button2);
         panel.add(button3);
+        panel.add(button4);
+        panel.add(button5);
+        panel.add(button6);
+        panel.add(button7);
+        panel.add(button8);
+        panel.add(button9);
+
         frame.add(panel);
         frame.setSize(200, 300);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
-        ArrayList<Integer> buttons = new ArrayList<Integer>(); // Create an ArrayList object
-        buttons.add(1);
-        buttons.add(2);
-        buttons.add(3);
-        buttons.add(4);
-        buttons.add(5);
-        buttons.add(6);
-        buttons.add(7);
-        buttons.add(8);
-        buttons.add(9);
-
-        System.out.println(buttons);
     }
 
 }
