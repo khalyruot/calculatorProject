9ecd7a32e434800e0fcc1d2f5e7cc6a85244a54d
Khaly_Ruot <63812639+khalyruot@users.noreply.github.com>
2023-01-16 08:33:55 -0800

    add multiple button

--- calculator.java
+++ calculator.java
@@ -1,30 +1,34 @@
 
-import java.awt.FlowLayout;  
-import javax.swing.JButton;  
-import javax.swing.JFrame;  
-import javax.swing.JLabel;  
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.util.ArrayList; // import the ArrayList class
 
-public class calculator{
-    public static void main(String[] arg){
- 
-        JFrame frame = new JFrame("Calculator");  
-        JPanel panel = new JPanel();  
-        panel.setLayout(new FlowLayout());  
-        JLabel label = new JLabel("Calculator");  
-        JButton button = new JButton();  
-        button.setText("1");  
-        button.setText("2"); 
-        panel.add(label);  
-        panel.add(button);  
-        frame.add(panel);  
-        frame.setSize(200, 300);  
-        frame.setLocationRelativeTo(null);  
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-        frame.setVisible(true);  
-            
-    
+public class calculator {
+    public static void main(String[] arg) {
+
+        JFrame frame = new JFrame("Calculator");
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        JLabel label = new JLabel("Calculator");
+        JButton button1 = new JButton();
+        JButton button2 = new JButton();
+        JButton button3 = new JButton();
+        button1.setText("1");
+        button2.setText("2");
+        button3.setText("3");
+        panel.add(label);
+        panel.add(button1);
+        panel.add(button2);
+        panel.add(button3);
+        frame.add(panel);
+        frame.setSize(200, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
         ArrayList<Integer> buttons = new ArrayList<Integer>(); // Create an ArrayList object
         buttons.add(1);
         buttons.add(2);
@@ -35,7 +39,7 @@ public class calculator{
         buttons.add(7);
         buttons.add(8);
         buttons.add(9);
-    
+
         System.out.println(buttons);
     }
 
